a3ec4eb39587c03d3d14c6cfb18ef55e9b273889
yzjlb <2541012655@qq.com>
2019-10-25 21:29:28 +0800

    修复imei获取问题 新增获取各种路径 更新运行器

--- src/main/java/com/xl/game/tool/Tool.java
+++ src/main/java/com/xl/game/tool/Tool.java
@@ -9,9 +9,11 @@
 package com.xl.game.tool;
 
 import android.content.*;
+import android.net.Uri;
 import android.telephony.*;
 import android.util.*;
 import java.io.*;
+import java.util.UUID;
 
 import android.util.Log;
 import android.os.Build;
@@ -33,7 +35,7 @@ public class Tool
 			//DataInputStream r12_DataInputStream = r9_DataInputStream;
 			DataInputStream r10_DataInputStream = new DataInputStream(context.getResources().getAssets().open(mapname));
 			input=r10_DataInputStream;
-			
+
 			r2_int_A_A= new int[input.readInt()][input.readInt()];
 			/*
 			r2_int_A_A=(int[][] )
@@ -45,7 +47,7 @@ public class Tool
 			);
 			*/
 			Log.e("XL", ""+r2_int_A_A[0].length );
-			
+
 			for(y=0;y<r2_int_A_A.length;y++)
 			{
 				for(x=0;x<r2_int_A_A[y].length;x++)
@@ -53,30 +55,7 @@ public class Tool
 					r2_int_A_A[y][x]=input.readInt();
 				}
 			}
-			
-			/*
-			y=0;
-			while(y<r2_int_A_A.length)
-			{
-				int x = 0;
-				
-				while(true)
-				{
-					
-					if(x>=r2_int_A_A[y].length)
-					{
-						y+=1;
-						break;
-					}
-					else
-					{
-						r2_int_A_A[y][x]=input.readInt();
-						x+=1;
-					}
-				}
-			}
-			
-			*/
+
 		}
 		catch(Exception e)
 		{
@@ -84,16 +63,61 @@ public class Tool
 		}
 		return r2_int_A_A;
 	}
-	
+
 	//获取imei码
+	/*
+
+	 */
 	public static String getImei(Context context)
 	{
 		TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-		return tm.getDeviceId();
+		if(Build.VERSION.SDK_INT<=28)
+			return tm.getDeviceId();
+		else
+			return getUUID();
 		// activity.getSystemService(Context.TELEPHONY_SERVICE).getDeviceId();
 //	String myIMSI=android.os.SystemProperties.get(android.telephony.TelephonyProperties.PROPERTY_IMSI);
 	}
 
+	public static String getUUID() {
+
+		String serial = null;
+
+		String m_szDevIDShort = "35" +
+				Build.BOARD.length() % 10 + Build.BRAND.length() % 10 +
+
+				Build.CPU_ABI.length() % 10 + Build.DEVICE.length() % 10 +
+
+				Build.DISPLAY.length() % 10 + Build.HOST.length() % 10 +
+
+				Build.ID.length() % 10 + Build.MANUFACTURER.length() % 10 +
+
+				Build.MODEL.length() % 10 + Build.PRODUCT.length() % 10 +
+
+				Build.TAGS.length() % 10 + Build.TYPE.length() % 10 +
+
+				Build.USER.length() % 10; //13 位
+
+		try {
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+				serial = android.os.Build.getSerial();
+			} else {
+				serial = Build.SERIAL;
+			}
+			//API>=9 使用serial号
+			return new UUID(m_szDevIDShort.hashCode(), serial.hashCode()).toString();
+		} catch (Exception exception) {
+			//serial需要一个初始化
+			serial = "serial"; // 随便一个初始化
+		}
+		//使用硬件信息拼凑出来的15位号码
+		return new UUID(m_szDevIDShort.hashCode(), serial.hashCode()).toString();
+	}
+
+
+	/*
+
+	 */
 	//获取imsi码
 	public static String getImsi(Context context)
 	{
@@ -101,11 +125,11 @@ public class Tool
 		return tm.getSubscriberId();
 
 	}
-	
+
 	//设置剪切板内容
 	public static void clipSet(Context context,CharSequence text)
 	{
-	if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB)
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB)
 		{
 			android.text.ClipboardManager clipboardManager = (android.text.ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
 			clipboardManager.setText(text);
@@ -116,7 +140,7 @@ public class Tool
 			clipboardManager.setText(text);
 		}
 	}
-	
+
 	//获取剪切板内容
 	public static CharSequence clipGet(Context context)
 	{
@@ -140,6 +164,5 @@ public class Tool
 			return clipboardManager.getText();
 		}
 	}
-	
-}
 
+}
